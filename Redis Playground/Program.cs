648a904ea18b1c6ef9056bf16b99f377386af70c
Delton Fernandes <Delton.Fernandes@british-business-bank.co.uk>
2024-11-11 15:24:21 +0000

    New projects added

---
+++ Redis Playground/Program.cs
@@ -0,0 +1,7 @@
+﻿using StackExchange.Redis;
+
+
+ConnectionMultiplexer redis = ConnectionMultiplexer.Connect("localhost");
+IDatabase db = redis.GetDatabase();
+db.StringSet("first key", "first value");
+redis.Close();
